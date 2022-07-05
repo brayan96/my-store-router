b1d4e3b6a27583b3df46a3a06961a19b8a3d9908
Brayan Steven Clavijo Beltrán <bclavijo@asesoftware.com>
2022-07-05 13:58:56 -0500

    Proyecto finalizado

---
+++ src/app/website/pages/register/register.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { OnExit } from './../../../guards/exit.guard';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss'],
+})
+export class RegisterComponent implements OnExit {
+  constructor() {}
+
+  onExit() {
+    const rta = confirm('Lógica desde comp, ¿estas seguro de salir?');
+    return rta;
+  }
+}
